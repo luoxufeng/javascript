ebf7f8ee3b2d35bc849e214c2031be88a52ba28a
xfluo <xfluo@Ctrip.com>
2015-11-02 17:50:41 +0800

    新添加的backbone

---
+++ JsDemo/backbone/2015-10-30/Demo/server/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace backbone._2015_10_30.Demo.server
+{
+    public class Book
+    {
+        public string Title { get; set; }
+
+        public string Id { get; set; }
+    }
+}
---
+++ JsDemo/backbone/2015-10-30/Demo/server/HandlerBook.ashx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Serialization;
+
+namespace backbone._2015_10_30.Demo.server
+{
+    /// <summary>
+    /// Summary description for HandlerBook
+    /// </summary>
+    public class HandlerBook : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+           // string jsonStr = "[{\"title\":\"书本1\"},{\"title\":'书本2'}]";
+           // context.Response.Write(jsonStr);
+            var book = context.Request["title"];
+
+            List<Book> books=new List<Book>();
+            books.Add(new Book(){Id = "1",Title = "语文"});
+            books.Add(new Book() { Id = "2", Title = "数学" });
+            books.Add(new Book() { Id = "3", Title = "英语" });
+            JavaScriptSerializer serializer=new JavaScriptSerializer();
+            string strJson= serializer.Serialize(books);
+            context.Response.Write(strJson);
+            context.Response.End();
+            
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
